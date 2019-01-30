ecadfb70503a479ef0c19bd47f8228cca773ae23
NiallS4 <44320530+NiallS4@users.noreply.github.com>
2019-01-30 15:47:35 +0000

    Add files via upload

--- lab_2.1/palindrome_021.py
+++ lab_2.1/palindrome_021.py
@@ -1,16 +1,16 @@
 import sys
 
 def palindrome(s):
+	s = s.strip().lower()
+	for letter in s:
+		if not letter.isalnum():
+			s = s.replace(letter, '')
 	rev_s = s[::-1]
 	return s == rev_s
 
 def main():
 	for line in sys.stdin:
-		s = line.strip().lower()
-		for letter in s:
-			if not letter.isalnum():
-				s = s.replace(letter, '')
-		print(palindrome(s))
+		print(palindrome(line))
 
 if __name__ == '__main__':
 	main()
